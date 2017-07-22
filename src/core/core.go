862e405c4cdf4b571eba58db563b0d37a0fc7c79
Pavel Vershinin <xr.pavel@yandex.ru>
2017-07-23 00:19:42 +0300

    upd

---
+++ src/core/core.go
@@ -0,0 +1,52 @@
+package core
+
+import "github.com/xrcms/globals"
+
+/**
+  Если какие либо из интерфейсов, вам точно не потребуются можете их закомментировать.
+  Сэкономите, пару микросекунд, спасёте дерево! ;)
+*/
+
+type Core struct {
+	Files globals.Fileser
+	Comments globals.Commentser
+	Infoblocks globals.Infoblocker
+	Groups globals.Groupser
+	Options globals.Optionser
+	Users globals.Userser
+	Session globals.Sessioner
+	Cookies globals.Cookieser
+	Output globals.Outputer
+	Permissions globals.Permissionser
+	Email globals.Emailer
+	EmailTemplates globals.EmailTemplateser
+	Request globals.Requester
+	DB globals.DataBaser
+	Pages globals.Pageer
+	Cache globals.Cacher
+	Templates globals.Templateser
+}
+
+func Init(ins []interface{}) *Core {
+	var c = new(Core)
+
+	c.Files          = ins[0].(globals.Fileser)
+	c.Comments       = ins[1].(globals.Commentser)
+	c.Infoblocks     = ins[2].(globals.Infoblocker)
+	c.Groups         = ins[3].(globals.Groupser)
+	c.Options        = ins[4].(globals.Optionser)
+	c.Users          = ins[5].(globals.Userser)
+	c.Session        = ins[6].(globals.Sessioner)
+	c.Cookies        = ins[7].(globals.Cookieser)
+	c.Output         = ins[8].(globals.Outputer)
+	c.Permissions    = ins[9].(globals.Permissionser)
+	c.Email          = ins[10].(globals.Emailer)
+	c.EmailTemplates = ins[11].(globals.EmailTemplateser)
+	c.Request        = ins[12].(globals.Requester)
+	c.DB             = ins[13].(globals.DataBaser)
+	c.Pages          = ins[14].(globals.Pageer)
+	c.Cache          = ins[15].(globals.Cacher)
+	c.Templates      = ins[16].(globals.Templateser)
+
+	return c
+}
---
+++ src/template.go
@@ -0,0 +1,69 @@
+/**
+	Шаблон модуля для XR.CMS
+	Начните с функций public(*core.Core) и admin(*core.Core)
+	Функция public должна возвращать HTML код публичной части модуля,
+	функция admin, соответственно, HTML код административной части модуля
+	По завершении разработки соберите ваш модуль командой:
+	go build -buildmode=plugin -o my_module_name.so src/template.go
+ */
+package main
+
+import (
+	"./core"
+	"encoding/json"
+	"fmt"
+	"github.com/xrcms/globals"
+)
+
+// Описание модуля
+var manifest, _ = json.Marshal(globals.Manifest{
+	Name: "Шаблон модуля",
+	Icon: "fa-font",
+	Version: "1.00.00",
+	ReleaseDate: "2017-07-23",
+	Description: "Заготовка модуля для XR.CMS",
+	Author: globals.Author {
+		Name: "Вершинин Павел Николаевич",
+		Email: "feedback@xr-cms.ru",
+		Site: "https://xr-cms.ru",
+	},
+	HomePage: "https://xr-cms.ru",
+	MinimumCmsVersion: "1.01.000",
+})
+
+func Init(arguments ...interface{}) []byte {
+	var entryPoint = arguments[0].(string)
+
+	// Программа запросила информацию о модуле
+	// Сразу, ничего не делая, вернём наш манифест
+	if entryPoint == "manifest" {
+		return manifest
+	}
+
+	// Подготовим ядро API
+	var c = core.Init(arguments[1:])
+
+	switch entryPoint {
+		case "public": // Запрошена публичная часть модуля, за неё отвечает функция public(*core.Core)
+			return public(c)
+		case "admin": // Запрошена административная часть модуля, за неё отвечает функция admin(*core.Core)
+			return admin(c)
+		// ...
+		// Любые другие, если необходимо, точки входа, дописываем тут, по аналогии с public и admin
+	}
+
+	return []byte(fmt.Sprintf(`Неверная точка входа "%s"`, entryPoint))
+}
+
+func public(core *core.Core) []byte {
+	// @TODO Код необходимый для пользовательской части модуля
+	return nil
+}
+
+func admin(core *core.Core) []byte {
+	if allow, err := core.Permissions.Check("admin/modules", core.Session.User().Groups); !allow {
+		return []byte(err.Error())
+	}
+	// @TODO Код, необходимый для административной части модуля, если модулю админка не нужна, можно оставить так
+	return nil
+}
